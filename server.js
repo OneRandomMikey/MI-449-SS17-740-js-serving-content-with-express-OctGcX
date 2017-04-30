807defc59855e3c95b1104bdb651b1ebfa5cbcf5
OneRandomMikey <zehua.li6@gmail.com>
2017-04-30 16:09:00 -0400

    create one temple for all data pages and use forEach fucntion to get the value

--- server.js
+++ server.js
@@ -13,19 +13,22 @@ function createHero (hero) {
 createHero({
   title: 'Ember',
   link: '/ember',
-  image: 'http://3.bp.blogspot.com/-9KBz_QTT3yA/U7JM2Y935gI/AAAAAAAADeE/Aj5zoSi0Fck/s1600/Dota+2+Ember+Spirit.jpg'
+  image: 'http://3.bp.blogspot.com/-9KBz_QTT3yA/U7JM2Y935gI/AAAAAAAADeE/Aj5zoSi0Fck/s1600/Dota+2+Ember+Spirit.jpg',
+  pages: 'pages/ember'
 })
 
 createHero({
   title: 'Riki',
   link: '/riki',
-  image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTJBOe-e2oDoYcK58qiTwMvYkjDpQ5CFo9g-4GMCE25MzV7kXS0pw'
+  image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTJBOe-e2oDoYcK58qiTwMvYkjDpQ5CFo9g-4GMCE25MzV7kXS0pw',
+  pages: 'pages/riki'
 })
 
 createHero({
   title: 'Weaver',
   link: '/weaver',
-  image: 'http://dotamods.ovh/wp-content/images/weaver/master.jpg'
+  image: 'http://dotamods.ovh/wp-content/images/weaver/master.jpg',
+  pages: 'pages/weaver'
 })
 
 app.set('view engine', 'ejs')
@@ -37,21 +40,12 @@ app.get('/', function (request, response) {
   })
 })
 
-app.get('/ember', function (request, response) {
-  response.render('pages/ember', {
-    heros: heros
-  })
-})
-
-app.get('/riki', function (request, response) {
-  response.render('pages/riki', {
-    heros: heros
-  })
-})
-
-app.get('/weaver', function (request, response) {
-  response.render('pages/weaver', {
-    heros: heros
+Object.keys(heros).forEach(function (id) {
+  var hero = heros[id]
+  app.get(hero.link, function (request, response) {
+    response.render(hero.pages, {
+      hero: hero
+    })
   })
 })
 
